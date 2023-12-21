66e0dba56cd5975d8a36b7bd1f5f8b1bfda690eb
NenavizhuSumerki <146851676+NenavizhuSumerki@users.noreply.github.com>
2023-12-21 15:13:38 +0300

    Update Program.cs

--- Avto2/Program.cs
+++ Avto2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Avto2;
 namespace Avto2
 {
@@ -8,11 +8,16 @@ namespace Avto2
         {
             Avto carAuto = new Avto();
             Console.WriteLine("Количество топлива: ");
-            carAuto.info("1ААА23", float.Parse(Console.ReadLine()), 10.0F);
-            carAuto.outinfo();
+            Random rand = new Random();
+            int value = rand.Next(0, 3);
+            string[] mas = new string[4] { "Вишнёвая семерка", "Мерседес", "Матиз", "Ока" };
+           
+
+            carAuto.info(mas[value], float.Parse(Console.ReadLine()), 10);
+            carAuto.Outinfo();
             Random random = new Random();
             int a = random.Next(1, 200);
-            Console.WriteLine($"До кабинета 1.11 (ДАЛЬШЕ БОГА НЕТ, до него осталось {a} км)");//эту часть кода придумал не я
+            Console.WriteLine($"До точки назначения осталось {a} км");
             carAuto.move(km: a);
         }
     }
